90801db202647b8948864002fa9d4e42025b0166
juber-ef <juber_ef@hotmail.com>
2025-02-21 15:52:54 -0300

    Addes selectors list for login spec

--- cypress/e2e/loginspec.cy.js
+++ cypress/e2e/loginspec.cy.js
@@ -1,15 +1,29 @@
 describe("Orange HRM Tests", () => {
+  const selectorsList = {
+    usernameField: "[name= 'username']",
+    passwordField: "[name='password']",
+    loginButton: "[type='submit']",
+    sectionTitleTopBar: ".oxd-topbar-header-breadcrumb-module",
+    wrongCredential: "[role='alert']",
+  };
+
   it("lOGIN - Sucess", () => {
     cy.visit(
       "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
     );
-    cy.get(
-      ":nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input"
-    ).type("Admin");
-    cy.get(
-      ":nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input"
-    ).type("admin123");
-    cy.get(".oxd-button").click();
+    cy.get(selectorsList.usernameField).type("Admin");
+    cy.get(selectorsList.passwordField).type("admin123");
+    cy.get(selectorsList.loginButton).click();
     cy.location("pathname").should("equal", "/web/index.php/dashboard/index");
+    cy.get(selectorsList.sectionTitleTopBar).contains("Dashboard");
+  });
+  it("lOGIN - Fail", () => {
+    cy.visit(
+      "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
+    );
+    cy.get(selectorsList.usernameField).type("Test");
+    cy.get(selectorsList.passwordField).type("test");
+    cy.get(selectorsList.loginButton).click();
+    cy.get(selectorsList.wrongCredential);
   });
 });
