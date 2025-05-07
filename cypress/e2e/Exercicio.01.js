fdf63a8a6d838adc392cc142e2b190cc46d2e020
juber-ef <juber_ef@hotmail.com>
2025-05-07 20:21:44 -0300

    Exercicios

--- cypress/e2e/Exercicio.01.js
+++ cypress/e2e/Exercicio.01.js
@@ -1,5 +1,53 @@
+import userData1 from "../fixtures/user-userData1";
 describe("Login com sucesso", () => {
+  const selectorsList = {
+    usernameField: "[#username= 'username' ]",
+    passswordField: "[#password= 'userpassword']",
+    loginButton: "[button= 'button']",
+    signUp: "[data-test='signup']",
+    nameUser: "#firstName",
+    lastName: "#lastName",
+    userName: "#username",
+    passwordEnter: "#password",
+    confirmPassword: "#confirmPassword",
+    login: ".SignUpForm-form",
+  };
+
   it("Deve fazer login com um usuário válido", () => {
     cy.visit("http://localhost:3000/signin").click();
+    cy.get(selectorsList.usernameField).type(userData1.userSuccess.username);
+    cy.get(selectorsList.passswordField).type(userData1.userSuccess.passsword);
+    cy.get(selectorsList.loginButton).click();
+  });
+});
+describe("Tentar fazer login com credenciais inválidas", () => {
+  it("Deve exibir uma mensagem de erro ao fazer login com credenciais inválidas", () => {
+    cy.visit("http://localhost:3000/signin").click();
+    cy.get(selectorsList.usernameField).type(userData1.userFail.username);
+    cy.get(selectorsList.passswordField).type(userData1.userFail.passsword);
+    cy.get(selectorsList.loginButton).click();
+  });
+});
+describe("Registro de novo usuário com sucesso", () => {
+  it("Deve registrar um novo usuário com informações válidas", () => {
+    cy.get(signUp).click();
+    cy.get(nameUser).type(junior);
+    cy.get(lastName).type(souza);
+    cy.get(userName).type(juju);
+    cy.get(passwordEnter).type(12345678);
+    cy.get(confirmPassword).type(12345678);
+    cy.get(login).click();
+  });
+});
+
+describe("Tentar registrar um novo usuário com informações incompletas", () => {
+  it("Deve exibir mensagens de erro ao tentar registrar um novo usuário sem preencher todas as informações obrigatórias", () => {
+    cy.get(signUp).click();
+    cy.get(nameUser).type(junior);
+    cy.get(lastName).type();
+    cy.get(userName).type(juju);
+    cy.get(passwordEnter).type(123);
+    cy.get(confirmPassword).type(1234);
+    cy.get(login).click();
   });
 });
--- cypress/e2e/Exercicio.02.js
+++ cypress/e2e/Exercicio.02.js
@@ -1,16 +1,29 @@
-describe('Enviar dinheiro com saldo suficiente', () => {
-    it('Deve enviar dinheiro com sucesso', () => {
-      cy.get(.MuiButton-colorInherit).click();
-      cy.get(#user-list-search-input).click().type(Parisian).enter();
-      cy.get([data-test='user-list-item-uBmeaz5pX'] .MuiListItemText-primary).click();
-      
-
-
-  
-  
-  
-  
-    });
-  
-  
+describe("Enviar dinheiro com saldo suficiente", () => {
+  it("Deve enviar dinheiro com sucesso", () => {
+    cy.get(".MuiButton-colorInherit").click();
+    cy.get("#user-list-search-input).click().type(Parisian").click();
+    cy.get(
+      "[data-test='user-list-item-uBmeaz5pX'] .MuiListItemText-primary"
+    ).click();
+    cy.get("#amount").type(1600);
+    cy.get(
+      ".MuiInputBase-input[aria-describedby='transaction-create-description-input-helper-text']"
+    ).type("PG");
+    cy.get("[data-test='transaction-create-submit-payment']").click();
   });
+});
+
+describe("Enviar dinheiro com saldo insuficiente", () => {
+  it("Deve exibir mensagem de erro ao enviar dinheiro sem saldo suficiente", () => {
+    cy.get(".MuiButton-colorInherit").click();
+    cy.get("#user-list-search-input).click().type(Parisian").click();
+    cy.get(
+      "[data-test='user-list-item-uBmeaz5pX'] .MuiListItemText-primary"
+    ).click();
+    cy.get("#amount").type(600);
+    cy.get(
+      ".MuiInputBase-input[aria-describedby='transaction-create-description-input-helper-text']"
+    ).type("PG");
+    cy.get("[data-test='transaction-create-submit-payment']").click();
+  });
+});
